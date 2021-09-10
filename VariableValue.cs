4f1dbad5138dc9f4a81c9240b87dfc8159eeb960
くり太郎 <62167170+kuritaro1122@users.noreply.github.com>
2021-09-10 11:25:35 +0900

    Add files via upload

---
+++ VariableValue.cs
@@ -0,0 +1,127 @@
+﻿using UnityEngine;
+//using VirtualTransform;
+
+namespace VariableValue {
+    [System.Serializable]
+    public abstract class VariableValue<ValueType> {
+        public enum ChangeType { none, addition, selection }
+        public enum LimitType { noLimit, turnBack, stop }
+        protected delegate ValueType ValueAdd(int index);
+        [SerializeField] internal ValueType defaultValue = default;
+        [SerializeField] internal ChangeType changeType = ChangeType.addition;
+        [SerializeField] internal ValueType addValue = default;
+        [SerializeField] internal float magnification = 1f;
+        [SerializeField, Range(0, 50)] internal int everyNTimes = 0;
+        [SerializeField] internal LimitType limitType = LimitType.noLimit;
+        [SerializeField, Range(0, 100)] internal int maxIndex = 0;
+        [SerializeField] internal ValueType[] selection = new ValueType[1];
+        //private int selectionSize;
+        protected ValueAdd valueAdd = delegate (int _index) {
+            Debug.LogErrorFormat("Warning/VariableValue/初期化されていません");
+            return default;
+        };
+        public VariableValue(ValueType defaultValue, ValueType addValue, float magnification = 1f, int everyNTimes = 0, LimitType limitType = LimitType.noLimit, int maxIndex = 0) {
+            this.changeType = ChangeType.addition;
+            this.defaultValue = defaultValue;
+            this.addValue = addValue;
+            this.magnification = magnification;
+            this.everyNTimes = everyNTimes;
+            this.limitType = limitType;
+            this.maxIndex = maxIndex;
+        }
+        public VariableValue(ValueType defaultValue, ValueType[] selection, int everyNTimes = 0, LimitType limitType = LimitType.noLimit, int maxIndex = 0) {
+            this.changeType = ChangeType.selection;
+            this.defaultValue = defaultValue;
+            this.selection = selection;
+            this.everyNTimes = everyNTimes;
+            this.limitType = limitType;
+            this.maxIndex = maxIndex;
+        }
+        public VariableValue() { }
+        public virtual ValueType GetValue(int index) {
+            int _index = GetLimitedIndex(index / (everyNTimes + 1));
+            switch (changeType) {
+                case ChangeType.none:
+                    return defaultValue;
+                case ChangeType.addition:
+                    return valueAdd(_index);
+                case ChangeType.selection:
+                    if (selection.Length > _index) return selection[_index];
+                    else {
+                        Debug.LogWarningFormat("Warning/VariableValue/Index out of Selection length");
+                        return defaultValue;
+                    }
+                default:
+                    Debug.LogWarningFormat("ChangeTypeError : " + changeType);
+                    return defaultValue;
+            }
+        }
+        private int GetLimitedIndex(int index) {
+            switch (limitType) {
+                default:
+                case LimitType.noLimit:
+                    return index;
+                case LimitType.turnBack:
+                    return index % (maxIndex + 1);
+                case LimitType.stop:
+                    return Mathf.Min(index, maxIndex);
+            }
+        }
+    }
+    [System.Serializable]
+    public class VariableFloat : VariableValue<float> {
+        public VariableFloat(float defaultValue, float addValue, float magnification = 1f, int evetyNTimes = 0, LimitType limitType = LimitType.noLimit, int maxIndex = 0) :
+            base(defaultValue, addValue, magnification, evetyNTimes, limitType, maxIndex) { }
+        public VariableFloat(float defaultValue, float[] selection, int everyNTimes = 0, LimitType limitType = LimitType.noLimit, int maxIndex = 0) :
+            base(defaultValue, selection, everyNTimes, limitType, maxIndex) { }
+        public VariableFloat() : base() { }
+        public override float GetValue(int index) {
+            valueAdd = delegate (int _index) { return defaultValue + magnification * addValue * _index; };
+            return base.GetValue(index);
+        }
+    }
+    [System.Serializable]
+    public class VariableVector2 : VariableValue<Vector2> {
+        public VariableVector2(Vector2 defaultValue, Vector2 addValue, float magnification = 1f, int evetyNTimes = 0, LimitType limitType = LimitType.noLimit, int maxIndex = 0) :
+            base(defaultValue, addValue, magnification, evetyNTimes, limitType, maxIndex) { }
+        public VariableVector2(Vector2 defaultValue, Vector2[] selection, int everyNTimes = 0, LimitType limitType = LimitType.noLimit, int maxIndex = 0) :
+            base(defaultValue, selection, everyNTimes, limitType, maxIndex) { }
+        public VariableVector2() : base() { }
+        public override Vector2 GetValue(int index) {
+            valueAdd = delegate (int _index) { return defaultValue + magnification * addValue * _index; };
+            return base.GetValue(index);
+        }
+    }
+    [System.Serializable]
+    public class VariableVector3 : VariableValue<Vector3>/*, IVector3Class<int>*/ {
+        public VariableVector3(Vector3 defaultValue, Vector3 addValue, float magnification = 1f, int evetyNTimes = 0, LimitType limitType = LimitType.noLimit, int maxIndex = 0) :
+            base(defaultValue, addValue, magnification, evetyNTimes, limitType, maxIndex) { }
+        public VariableVector3(Vector3 defaultValue, Vector3[] selection, int everyNTimes = 0, LimitType limitType = LimitType.noLimit, int maxIndex = 0) :
+            base(defaultValue, selection, everyNTimes, limitType, maxIndex) { }
+        public VariableVector3() : base() { }
+        public override Vector3 GetValue(int index) {
+            valueAdd = delegate (int _index) { return defaultValue + magnification * addValue * _index; };
+            return base.GetValue(index);
+        }
+        public Vector3 GetVector3(int value) => GetValue(value);
+    }
+    [System.Serializable]
+    public class VariableVector4 : VariableValue<Vector4> {
+        public VariableVector4(Vector4 defaultValue, Vector4 addValue, float magnification = 1f, int evetyNTimes = 0, LimitType limitType = LimitType.noLimit, int maxIndex = 0) :
+            base(defaultValue, addValue, magnification, evetyNTimes, limitType, maxIndex) { }
+        public VariableVector4(Vector4 defaultValue, Vector4[] selection, int everyNTimes = 0, LimitType limitType = LimitType.noLimit, int maxIndex = 0) :
+            base(defaultValue, selection, everyNTimes, limitType, maxIndex) { }
+        public VariableVector4() : base() { }
+        public override Vector4 GetValue(int index) {
+            valueAdd = delegate (int _index) { return defaultValue + magnification * addValue * _index; };
+            return base.GetValue(index);
+        }
+    }
+    [System.Serializable]
+    public class VariableQuaternion : VariableValue<Quaternion> {
+        public override Quaternion GetValue(int index) {
+            valueAdd = delegate (int _index) { return defaultValue * Quaternion.LerpUnclamped(Quaternion.identity, addValue, magnification * _index); };
+            return base.GetValue(index);
+        }
+    }
+}
---
+++ VariableValueDrawer.cs
@@ -0,0 +1,150 @@
+﻿//using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using VariableValue;
+
+#if UNITY_EDITOR
+[CustomPropertyDrawer(typeof(VariableFloat))]
+public class VariableFloatDrawer : VariableValueDrawer {}
+[CustomPropertyDrawer(typeof(VariableVector2))]
+public class VariableVector2Drawer : VariableValueDrawer {}
+[CustomPropertyDrawer(typeof(VariableVector3))]
+public class VariableVector3Drawer : VariableValueDrawer {}
+
+//[CustomPropertyDrawer(typeof(VariableValue<Vector2>))]
+//public class VariableValue1Drawer : VariableValueDrawer { }
+
+//[CustomPropertyDrawer(typeof(VariableValue<float>))]
+//public class VariableValueDrawer_float : VariableValueDrawer {}
+//[CustomPropertyDrawer(typeof(VariableVector4))]
+//public class VariableVector4Drawer : VariableValueDrawer<Vector4> {}
+
+public class VariableValueDrawer/*<ValueType>*/ : PropertyDrawer {
+
+    private class PropertyData {
+        public SerializedProperty defaultValueProperty;
+        public SerializedProperty changeTypeProperty;
+        public SerializedProperty addValueProperty;
+        public SerializedProperty magnificationProperty;
+        public SerializedProperty everyNTimesProperty;
+        public SerializedProperty limitTypeProperty;
+        public SerializedProperty maxIndexProperty;
+        public SerializedProperty selectionProperty;
+    }
+
+    private Dictionary<string, PropertyData> _propertyDataPerPropertyPath = new Dictionary<string, PropertyData>();
+    private PropertyData _property;
+
+    private float LineHeight { get { return EditorGUIUtility.singleLineHeight + EditorGUIUtility.standardVerticalSpacing; } }
+
+    private void Init(SerializedProperty property) {
+        if (_propertyDataPerPropertyPath.TryGetValue(property.propertyPath, out _property)) {
+            return;
+        }
+
+        _property = new PropertyData();
+        _property.defaultValueProperty = property.FindPropertyRelative("defaultValue");
+        _property.changeTypeProperty = property.FindPropertyRelative("changeType");
+        _property.addValueProperty = property.FindPropertyRelative("addValue");
+        _property.magnificationProperty = property.FindPropertyRelative("magnification");
+        _property.everyNTimesProperty = property.FindPropertyRelative("everyNTimes");
+        _property.limitTypeProperty = property.FindPropertyRelative("limitType");
+        _property.maxIndexProperty = property.FindPropertyRelative("maxIndex");
+        _property.selectionProperty = property.FindPropertyRelative("selection");
+        _propertyDataPerPropertyPath.Add(property.propertyPath, _property);
+    }
+
+    //int _height;
+    //Rect fieldRect;
+
+    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label) {
+        Init(property);
+        Rect fieldRect = position;
+        fieldRect.height = LineHeight;
+
+        using (new EditorGUI.PropertyScope(fieldRect, label, property)) {
+            property.isExpanded = EditorGUI.Foldout(new Rect(fieldRect), property.isExpanded, label);
+            if (property.isExpanded) {
+                using (new EditorGUI.IndentLevelScope()) {
+                    //EditorGUI.indentLevel++;
+                    PropertyFieldGUI(ref fieldRect, _property.defaultValueProperty);
+                    PropertyFieldGUI(ref fieldRect, _property.changeTypeProperty);
+                    EachOptionPropertyFieldGUI(ref fieldRect);
+                    //EditorGUI.indentLevel--;
+                }
+            } else MiniPreview(fieldRect);
+        }
+    }
+    public override float GetPropertyHeight(SerializedProperty property, GUIContent label) {
+        Init(property);
+        int height = 1;
+        if (property.isExpanded)
+            switch (_property.changeTypeProperty.enumValueIndex) {
+                case 0:
+                    height += 2;
+                    break;
+                case 1:
+                    height += 6;
+                    if (_property.limitTypeProperty.enumValueIndex != 0) height++;
+                    break;
+                case 2:
+                    height += 5;
+                    if (_property.selectionProperty.isExpanded) height += _property.selectionProperty.arraySize + 1;
+                    if (_property.limitTypeProperty.enumValueIndex != 0) height++;
+                    break;
+            }
+        return height * LineHeight;
+    }
+    //配列のみChildrenの表示対応
+    private void PropertyFieldGUI(ref Rect rect, SerializedProperty serializedProperty, bool includeChildren = false) {
+        rect.y += LineHeight;
+        EditorGUI.PropertyField(new Rect(rect), serializedProperty, includeChildren);
+        if (!includeChildren || !serializedProperty.isExpanded) return;
+        if (serializedProperty.isArray) rect.y += (serializedProperty.arraySize + 1) * LineHeight;
+    }
+    /*private void ArrayElementsPropertyFieldGUI(SerializedProperty serializedProperty) {
+        AddLineHeight();
+        GUIContent label = new GUIContent("Size");
+        serializedProperty.arraySize = EditorGUI.DelayedIntField(fieldRect, label, serializedProperty.arraySize);
+        for (int i = 0; i < serializedProperty.arraySize; i++) {
+            var selection = serializedProperty.GetArrayElementAtIndex(i);
+            PropertyFieldGUI(selection);
+        }
+    }*/
+    private void MiniPreview(Rect rect) {
+        Rect valueRect = new Rect(rect);
+        Rect changeTypeRect = new Rect(rect);
+        float middle = 0.9f;
+        valueRect.xMin += 100f;
+        valueRect.xMax = (rect.xMax - valueRect.xMin) * middle;
+        changeTypeRect.xMin = valueRect.xMax;
+        //Debug.Log((valueRect.xMin, valueRect.xMax));
+        GUI.enabled = false;
+        EditorGUI.PropertyField(valueRect, _property.defaultValueProperty, new GUIContent());
+        EditorGUI.PropertyField(changeTypeRect, _property.changeTypeProperty, new GUIContent());
+        GUI.enabled = true;
+    }
+    private void EachOptionPropertyFieldGUI(ref Rect rect) {
+        switch (_property.changeTypeProperty.enumValueIndex) {
+            case 0:
+                return;
+            case 1:
+                PropertyFieldGUI(ref rect, _property.addValueProperty);
+                PropertyFieldGUI(ref rect, _property.magnificationProperty);
+                break;
+            case 2:
+                SerializedProperty selectionList = _property.selectionProperty;
+                PropertyFieldGUI(ref rect, selectionList, true);
+                //EditorGUI.indentLevel++;
+                //if (selectionList.isExpanded) ArrayElementsPropertyFieldGUI(selectionList);
+                //EditorGUI.indentLevel--;
+                break;
+        }
+        PropertyFieldGUI(ref rect, _property.everyNTimesProperty);
+        PropertyFieldGUI(ref rect, _property.limitTypeProperty);
+        if (_property.limitTypeProperty.enumValueIndex != 0) //noLimitでない
+            PropertyFieldGUI(ref rect, _property.maxIndexProperty);
+    }   
+}
+#endif
